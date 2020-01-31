2f6ba6b5e6119fe8950ca4cb36e4ba0dd80576d3
student5308 <12068940+LilCurly@users.noreply.github.com>
2020-01-31 08:18:07 +0100

    Wallet handler for generating new wallets and loading existing ones

--- CryptoFunding/Utils/WalletHandler.swift
+++ CryptoFunding/Utils/WalletHandler.swift
@@ -7,3 +7,70 @@
 //
 
 import Foundation
+import Web3swift
+
+class WalletHandler {
+    
+    static func generateNewWallet(password: String, walletName: String) -> Wallet {
+        let repo = WalletRepository.instance
+        
+        let keystore = try! EthereumKeystoreV3(password: password)!
+        let keyData = try! JSONEncoder().encode(keystore.keystoreParams)
+        let address = keystore.addresses!.first!.address
+        let wallet = Wallet(address: address, data: keyData, name: walletName)
+        
+        repo.addWallet(wallet: wallet)
+        
+        return wallet
+    }
+    
+    static func generateWalletFromPrivateKey(privateKey: String, password: String, walletName: String) -> Wallet? {
+        let repo = WalletRepository.instance
+        
+        let formattedKey = privateKey.trimmingCharacters(in: .whitespacesAndNewlines)
+        guard let dataKey = Data.fromHex(formattedKey) else {return nil}
+        do {
+            guard let keystore = try EthereumKeystoreV3(privateKey: dataKey, password: password) else {return nil}
+            let keyData = try JSONEncoder().encode(keystore.keystoreParams)
+            let address = keystore.addresses!.first!.address
+            let wallet = Wallet(address: address, data: keyData, name: walletName)
+            
+            repo.addWallet(wallet: wallet)
+            
+            return wallet
+        } catch {
+            return nil
+        }
+    }
+    
+    static func loadCurrentWallet(password: String, walletAddress: String) -> Wallet? {
+        let repo = WalletRepository.instance
+        
+        guard let wallet = repo.getWalletForAddress(address: walletAddress) else {return nil}
+        guard let keystore = EthereumKeystoreV3(wallet.data) else {return nil}
+        
+        if !isPasswordCorrect(keystore: keystore, address: wallet.address, password: password) {
+            return nil
+        }
+        
+        repo.setCurrent(wallet: wallet)
+        return wallet
+    }
+    
+    static func loadWalletsFromStorage() -> [Wallet] {
+        let repo = WalletRepository.instance
+        let listOfWallet = repo.getAllWallets()
+        
+        return listOfWallet
+    }
+    
+    fileprivate static func isPasswordCorrect(keystore: EthereumKeystoreV3, address: String, password: String) -> Bool {
+        guard let ethAddress = EthereumAddress(address) else {return false}
+        do {
+            let _ = try keystore.UNSAFE_getPrivateKeyData(password: password, account: ethAddress)
+            return true
+        } catch {
+            return false
+        }
+    }
+}
