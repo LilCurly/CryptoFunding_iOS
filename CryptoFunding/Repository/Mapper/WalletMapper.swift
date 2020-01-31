1d86f9f4dc5641c94efb1d1724ce5ed2fa7656fc
student5308 <12068940+LilCurly@users.noreply.github.com>
2020-01-31 08:15:48 +0100

    Mapper for mapping Wallet comming from the DAO to the Wallet entity and the opposite

--- CryptoFunding/Repository/Mapper/WalletMapper.swift
+++ CryptoFunding/Repository/Mapper/WalletMapper.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+class WalletMapper {
+    
+    static func modelToData(model: Wallet) -> WalletData {
+        return WalletData(address: model.address, data: model.data, name: model.name)
+    }
+    
+    static func dataToModel(data: WalletData) -> Wallet {
+        return Wallet(address: data.address, data: data.data, name: data.name)
+    }
+    
+    static func dataListToModelList(datas: [WalletData]) -> [Wallet] {
+        var listOfWallets = [Wallet]()
+        
+        for data in datas {
+            listOfWallets.append(Wallet(address: data.address, data: data.data, name: data.name))
+        }
+        
+        return listOfWallets
+    }
+    
+    static func modelListToDataList(models: [Wallet]) -> [WalletData] {
+        var listOfWalletData = [WalletData]()
+        
+        for model in models {
+            listOfWalletData.append(WalletData(address: model.address, data: model.data, name: model.name))
+        }
+        
+        return listOfWalletData
+    }
+}
