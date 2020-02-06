0b02d8c9c3f3aaf14fc6adab583005176fa10f9b
student5308 <12068940+LilCurly@users.noreply.github.com>
2020-02-06 09:50:28 +0100

    adapting to contract refactoring

--- CryptoFunding/ViewController.swift
+++ CryptoFunding/ViewController.swift
@@ -14,17 +14,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-//        let receipt = try! Web3Util.instance.eth.getTransactionReceipt("0xe159c0af9743bf6517621d705dc9cbd79ec9493ad82ed619e68b66730f4a97a3")
-//        print(receipt.contractAddress?.address)
         let repoInstance = WalletRepositoryFactory.getFakeDaoRepository()
         let wallet = WalletHandler.generateWalletFromPrivateKey(privateKey: "7EB64DDB45D47DEF728B97D2B539B62CE7FCBF278D9610C96D8ED279A527FC96", password: "test", walletName: "MyWallet")!
         repoInstance.setCurrent(wallet: wallet)
         let currentWallet = WalletRepository.instance.getCurrent()
         if let currentWallet = currentWallet {
-            let result = SimpleTestContract.deploy(address: currentWallet.getAddress()!, password: "test")
-            if let result = result {
-                print(result.hash)
-                Web3Util.getTransactionReceipt(hash: result.hash)
+            let writeTx = SimpleTestContract.deploy(address: currentWallet.getAddress()!)
+            if let writeTx = writeTx {
+                let estimatedGasCost = Web3Util.estimateGasCost(tx: writeTx)
+                print(estimatedGasCost ?? "")
+                guard let sentTx = Web3Util.writeTransaction(tx: writeTx, password: "test") else {return}
+                print(sentTx.hash)
+                Web3Util.getTransactionReceipt(hash: sentTx.hash)
                 print("test")
             }
         }
