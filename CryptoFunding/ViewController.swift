91a408bcb047bcebaf538c7ca6b78c7c98c4626e
student5308 <12068940+LilCurly@users.noreply.github.com>
2020-02-06 08:29:27 +0100

    getting contract address after it's deployment

--- CryptoFunding/ViewController.swift
+++ CryptoFunding/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        let receipt = try! Web3Util.instance.eth.getTransactionReceipt("0xe159c0af9743bf6517621d705dc9cbd79ec9493ad82ed619e68b66730f4a97a3")
+//        print(receipt.contractAddress?.address)
         let repoInstance = WalletRepositoryFactory.getFakeDaoRepository()
         let wallet = WalletHandler.generateWalletFromPrivateKey(privateKey: "7EB64DDB45D47DEF728B97D2B539B62CE7FCBF278D9610C96D8ED279A527FC96", password: "test", walletName: "MyWallet")!
         repoInstance.setCurrent(wallet: wallet)
@@ -22,6 +24,8 @@ class ViewController: UIViewController {
             let result = SimpleTestContract.deploy(address: currentWallet.getAddress()!, password: "test")
             if let result = result {
                 print(result.hash)
+                Web3Util.getTransactionReceipt(hash: result.hash)
+                print("test")
             }
         }
         
