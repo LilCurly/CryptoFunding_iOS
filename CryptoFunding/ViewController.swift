0f7bce7be8339e542bb9b70e73b0036df90c09d5
student5308 <12068940+LilCurly@users.noreply.github.com>
2020-02-14 17:06:46 +0100

    examples

--- CryptoFunding/ViewController.swift
+++ CryptoFunding/ViewController.swift
@@ -15,12 +15,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let repoInstance = WalletRepositoryFactory.getFakeDaoRepository()
+        /* Exemple d'import de portefeuille avec une clé privée, la création d'un nouveau portefeuille n'est pas plus compliqué */
         let wallet = WalletHandler.generateWalletFromPrivateKey(privateKey: "7EB64DDB45D47DEF728B97D2B539B62CE7FCBF278D9610C96D8ED279A527FC96", password: "test", walletName: "MyWallet")!
         repoInstance.setCurrent(wallet: wallet)
         let currentWallet = WalletRepository.instance.getCurrent()
         if let currentWallet = currentWallet {
-            let result = FundingContract.getTotalTasks(userAddress: currentWallet.getAddress()!, projectId: 0)
-            print(result)
+            /* Exemple d'intéraction avec la blockchain en lecture -> Pas de coût en gas */
+//            let result = FundingContract.getTotalTasks(userAddress: currentWallet.getAddress()!, projectId: 0)
+//            print(result)
+            /* Exemple d'intéraction avec la blockchain en écriture -> Coût en gas, récupération de l'estimation du coût, écriture de la transaction avec le mdp du portefeuille et récupération de la recette à la fin de la transaction */
 //            let writeTx = FundingContract.launchFundingProject(userAddress: currentWallet.getAddress()!, taskNumber: 1, deadlines: [100000000000000000], amountAsked: [500])
 //            if let writeTx = writeTx {
 //                let estimatedGasCost = Web3Util.estimateGasCost(tx: writeTx)
